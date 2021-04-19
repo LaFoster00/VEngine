79e2eb5c702af066823c3b1fd54a4eeef521f57a
Lasse Foster <lasse.foster@web.de>
2021-04-19 20:18:08 +0200

    + restructuring and shader compilation

---
+++ VEngine/VEngine/compileHumanReadable.bat
@@ -0,0 +1,3 @@
+C:\VulkanSDK\1.2.170.0\Bin\glslc.exe .\src\Shaders\Basic.vert -S -O -o .\src\Shaders\Spir-V\vert.spv
+C:\VulkanSDK\1.2.170.0\Bin\glslc.exe .\src\Shaders\Basic.frag -S -O -o .\src\Shaders\Spir-V\frag.spv
+pause
